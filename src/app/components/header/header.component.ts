529a3f7e96b912ddce3f71ebbbb12117cdbcd46f
yassinemarzouki <92584248+yassinemarzouki@users.noreply.github.com>
2025-03-11 16:01:18 +0100

    tailwindcss

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { signal } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  title =signal('Welcome TO ChwayaShop');
+
+}
